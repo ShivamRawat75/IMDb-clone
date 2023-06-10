609ad546692587f8445ce0ef40553a6462c400c9
Shivam Rawat <ajsivamaj@gmail.com>
2023-06-10 13:47:11 +0530

    heder ui complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
 import { AppBar, Toolbar, styled, Box, Typography, InputBase } from '@mui/material';
 
-import { logoURL } from './constants/constant';
+import { logoURL } from '../constants/constant';
 import MenuIcon from '@mui/icons-material/Menu';
 import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import HeaderMenu from './HeaderMenu';
+
+import { useState } from 'react';
+
+
 const StyledToolbar = styled(Toolbar)(`
 
     background: #121212;
     minHeight: 56px !important;
     padding: 0 115px !important;
     justify-content:space-between;
+    & > * {
+        padding:0 16px;
+    }
     & >div {
         display:flex;
         align-item:center;
@@ -20,7 +29,7 @@ const StyledToolbar = styled(Toolbar)(`
 
         }
     }
-    & >p {
+    & > p {
         font-size:14px;
         font-weight:600;
 
@@ -29,20 +38,35 @@ const StyledToolbar = styled(Toolbar)(`
 
 `)
 
+const InppputSearchField=styled(InputBase)`
+    background:#ffffff;
+    hight:30px;
+    width:55%;
+    border-radius:5px;
+`
+
 const Logo = styled('img')({
     width: 64
 })
 
+const handleClick=()=>{
+    
+}
+
 const Header = () => {
+
+const [open,setOpen]=useState(null);
+
     return (
         <AppBar>
             <StyledToolbar>
                 <Logo src={logoURL} alt="logo" />
-                <Box>
+                <Box onClick={handleClick}>
                     <MenuIcon />
                     <Typography>Menu</Typography>
                 </Box>
-                <InputBase />
+                <HeaderMenu/>
+                <InppputSearchField />
                 <Typography>IMDb<Box component='span'>Pro</Box></Typography>
                 <Box>
                     <BookmarkAddIcon />
---
+++ src/components/HeaderMenu.jsx
@@ -0,0 +1,9 @@
+
+
+const HeaderMenu=()=>{
+    return(
+        <>Hello from headerMenu</>
+    )
+}
+
+export default HeaderMenu;
